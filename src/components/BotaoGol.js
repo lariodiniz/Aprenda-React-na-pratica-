00f6cf9e86ed0956e830cb95a72c5d55e5faa693
lariodiniz <lariodiniz@gmail.com>
2019-03-08 14:05:26 -0300

    Fim da Aula 07
    Link da Aula:
    https://www.youtube.com/watch?v=LzwE6vixNBU

---
+++ src/components/BotaoGol.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class BotaoGol extends React.Component {
+        
+    render(){
+        return (
+            
+            <button>Gol!</button>
+            
+        );
+    }
+}
---
+++ src/components/PlacarContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Time from './Time'
+import Partida from './Partida'
+
+export default class PlacarContainer extends React.Component {
+        
+    render(){
+        return (
+            <div>
+                <div style={{float:"left", "marginRight":"10px"}}>
+                    <h3>Casa</h3>
+                    <Time />
+                </div>
+                <div style={{float:"left", "marginRight":"10px"}}><Partida /></div>
+                <div style={{float:"left", "marginRight":"10px"}}>
+                    <h3>Visitante</h3>
+                    <Time />
+                </div>
+                <div style={{clear:"both"}}></div>
+            </div>
+
+        );
+    }
+}
